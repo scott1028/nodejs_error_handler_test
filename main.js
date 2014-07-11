cf8a7cc820806baf037447bc9716b426c19ecfa2
scott.lan <scott.lan@taisys.com>
2014-07-11 21:12:14 +0800

    error handle

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-var i = 1;
-setInterval(function(){
-	i += 1;
-	console.log(1);
+// main.js
 
-	if(i >= 3){
-		throw Error('User raise error!');
-	}
-}, 1000);
+var main = function(){
+    var i = 1;
+    setInterval(function(){
+        i += 1;
+        console.log(1);
+
+        if(i >= 3){
+            throw Error('User raise error!');
+        }
+    }, 1000);
+};
+
+process.on('uncaughtException', function(err) {
+    console.log('Caught exception: ' + err);
+    main();
+});
+
+main();
